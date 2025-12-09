867d5b55d200c9ff353f4937aeceaa720ee58f00
@yogesh.panjarkar <yogesh.panjarkar@epps-erp.in>
2025-12-09 14:31:28 +0530

    evnveronment variable added for openapi config

--- frontend/src/app/services/advertisement.service.ts
+++ frontend/src/app/services/advertisement.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 export interface Advertisement {
     id?: number;
     imageUrl: string;
@@ -13,7 +15,7 @@ export interface Advertisement {
     providedIn: 'root'
 })
 export class AdvertisementService {
-    private apiUrl = 'http://localhost:8080/api';
+    private apiUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) { }
 
